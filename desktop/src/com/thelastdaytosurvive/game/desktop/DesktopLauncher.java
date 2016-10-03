5bfd309dae675e599cdb61ea7c97358b594d00df
Pattnatt <jittapat007@gmail.com>
2016-10-03 20:57:11 +0700

    Create New Project

---
+++ desktop/src/com/thelastdaytosurvive/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.thelastdaytosurvive.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.thelastdaytosurvive.game.LastDayGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LastDayGame(), config);
+	}
+}
